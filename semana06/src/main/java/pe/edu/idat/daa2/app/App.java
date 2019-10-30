b7816f0173fff255f01753637439892adfbfe825
Dayana <a18104164@idat.edu.pe>
2019-10-30 11:57:26 -0500

    - Modificacion

--- semana06/src/main/java/pe/edu/idat/daa2/app/App.java
+++ semana06/src/main/java/pe/edu/idat/daa2/app/App.java
@@ -4,5 +4,7 @@ package pe.edu.idat.daa2.app;
 public class App {
     public static void main(String[] args){
         System.out.println("HOLA");
+        
+        System.out.println("Modificado por D");
     }
 }
